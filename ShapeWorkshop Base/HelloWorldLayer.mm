63ee65ea7f9c5a63e2533b5fd3404b652be5e105
Oleg <Oleg@Olegs-iMac.local>
2012-10-13 12:32:16 +0400

    1

--- ShapeWorkshop Base/HelloWorldLayer.mm
+++ ShapeWorkshop Base/HelloWorldLayer.mm
@@ -19,6 +19,7 @@
 
 @implementation HelloWorldLayer
 
+
 +(CCScene *) scene
 {
 	// 'scene' is an autorelease object.
@@ -41,7 +42,7 @@
         /*
          * Setting up the world
          */
-        
+        self.isAccelerometerEnabled = YES;
         // Get a link to the level config file
         NSString * path = [[NSBundle mainBundle] pathForResource:@"labyrinth" ofType:@"plist"];
         
@@ -318,7 +319,7 @@
     _ball.spriteLink.visible = NO;
 
     // Reset it's sprite to scale 1
-    //_ball.spriteLink.scale = 1;
+    _ball.spriteLink.scale = 1;
     
     // Get hold of the Box2D body
     NSValue * bodyValue = _ball.physicsLink;
@@ -327,7 +328,16 @@
     [bodyValue getValue:&ballBody];
     
     // Move it back to the start position
-    ballBody->SetTransform(_startPosition, 0);
+    b2Vec2 ballPos = ballBody->GetPosition();
+    NSLog(@"Position: %f, %f", ballPos.x, ballPos.y);
+    //NSLog(@"Sila: %f, %f", ballBody->GetLinearVelocity.Length(), _ballForce.y); //_ballForce.x!=0 && _ballForce.y
+    int xTransform;
+    if (ballBody->GetLinearVelocity().x < 0)
+    {
+        xTransform = 44;
+    } else {xTransform = -44;}
+    NSLog(@"Speed: %f, %f", ballBody->GetLinearVelocity().x, _ballForce.y); //_ballForce.x!=0 && _ballForce.y
+    ballBody->SetTransform(b2Vec2(ballPos.x + xTransform, ballPos.y), ballBody->GetAngle()); //
     
     // The animation will have change the sprite frame. To reset
     // this we need to set the displayed frame back to the original ball
@@ -355,7 +365,7 @@
     //id move = [CCMoveTo actionWithDuration:0.1  position:ccp(holePos.x + holeSize.width*0.1,
      //                                                      holePos.y + holeSize.height*0.1)];
     // Shrink the ball to 80% of it's normal size
-    //id shrink = [CCScaleTo actionWithDuration:0.1 scale:0.8];
+    id shrink = [CCScaleTo actionWithDuration:0.1 scale:0.8];
     
     // Setup the frame by frame rolling animation
     id animate = [CCAnimate  actionWithAnimation:[CCAnimation animationWithSpriteFrames:_ballAnimationFrames]];
@@ -391,23 +401,7 @@
     if(_runPhysics)
         [_builder update:dt];
     
-    // Check if the ball is over half in the hole
-    if(_runPhysics) {
-        CGPoint holePos = _hole.spriteLink.position;
-        CGPoint ballPos = _ball.spriteLink.position;
-        //NSLog(@"ball.position: %f, %f", _ball.spriteLink.position.x, _ball.spriteLink.position.y);
-        
-        // Calculate the distance of ball from the hole
-        float dist = sqrtf(powf(holePos.x - ballPos.x, 2) + powf(holePos.y - ballPos.y, 2));
-        
-        // If the distance is less than 50% of the balls width it will fall
-        // This is because in reality the contact surface of the ball on the 
-        // wood is very small and occurs at 50% of the balls width. Only when
-        // this contact area is in the hole will the ball fall.
-        if(ballPos.x > 850 || ballPos.x < 18) {
-            [self triggerFallAnimation];
-        }
-    }    
+
     
      
     b2Body * ballBody;
@@ -424,6 +418,32 @@
             ballBody->ApplyForce(_ballForce, ballBody->GetWorldCenter());
         }
     }
+    
+    // Check if the ball is over half in the hole
+    if(_runPhysics) {
+        CGPoint holePos = _hole.spriteLink.position;
+        CGPoint ballPos = _ball.spriteLink.position;
+        
+        //NSLog(@"ball.position: %f, %f", _ball.spriteLink.position.x, _ball.spriteLink.position.y);
+        
+        // Calculate the distance of ball from the hole
+        float dist = sqrtf(powf(holePos.x - ballPos.x, 2) + powf(holePos.y - ballPos.y, 2));
+        //NSLog(@"Sila: %f, %f", ballBody->GetLinearVelocity.Length(), _ballForce.y); //_ballForce.x!=0 && _ballForce.y
+        // If the distance is less than 50% of the balls width it will fall
+        // This is because in reality the contact surface of the ball on the
+        // wood is very small and occurs at 50% of the balls width. Only when
+        // this contact area is in the hole will the ball fall.
+    
+        //NSLog(@"Sila: %f, %f", ballBody->GetLinearVelocity().x, _ballForce.y); //_ballForce.x!=0 && _ballForce.y
+        if(ballPos.x < 25 && ballBody->GetLinearVelocity().x < 0) {
+            [self triggerFallAnimation];
+        }
+        if(ballPos.x > 875 && ballBody->GetLinearVelocity().x > 0) {
+            [self triggerFallAnimation];
+        }
+    }
+    
+    
     // Apply the repulsive effect of the fan
     
     if( _fan != Nil && _ball!= Nil) {
@@ -442,7 +462,8 @@
         
             // Inverse squared force - will be tiny at any great distance
             b2Vec2 force (forceFactor * (xComp / fabsf(xComp)) / distSq, forceFactor * (yComp/fabsf(xComp))/distSq);
-            
+
+
             if(force.x > 10) {
             //    force.x = 10;
             }
@@ -461,6 +482,23 @@
 }
 
 
-
+-(void) accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration {
+    
+    if(_ball != Nil) {
+    	// Get a pointer to the ball's physics body
+        b2Body * ballBody;
+        NSValue * bodyValue = _ball.physicsLink;
+        [bodyValue getValue:&ballBody];
+        
+        // Make sure the body is awake (if it's not awake it won't be affected by gravity)
+        ballBody->SetAwake(YES);
+    }
+    
+    // Set the gravity by the tilt angle
+    b2Vec2 gravity ( 40 * acceleration.y, - 40 * acceleration.x);
+    NSLog(@"Gravity: %f, %f", gravity.x, gravity.y);
+    _world->SetGravity(gravity);
+    NSLog(@"Acceleration: %f, %f, %f", acceleration.x, acceleration.y, acceleration.z);
+}
 
 @end
